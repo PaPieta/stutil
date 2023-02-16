b4322d171a8ac1338f47ed14da6605f78b76b8f8
PaPieta <s202606@student.dtu.dk>
2023-02-16 15:53:24 +0100

    Added function documentation

--- stutil/volume.py
+++ stutil/volume.py
@@ -9,7 +9,12 @@ from stutil import vtk_write_lite
 #TODO: update documentation
 
 def hsv2rgb3d(vol):
-    #https://www.had2know.org/technology/hsv-rgb-conversion-formula-calculator.html
+    """Converts a volume from a hsv to rgb definition, extension of the 2D version\n
+    Source:https://www.had2know.org/technology/hsv-rgb-conversion-formula-calculator.html\n
+    Params:\n
+    vol - (3,x,y,z) color (hsv) volume\n
+    Returns: (3,x,y,z) rgb volume"""
+    #
     M = vol[2,:,:,:]
     m = M*(1-vol[1,:,:,:])
     mod_res = ((vol[0,:,:,:]*180/(np.pi*60)))%2
@@ -54,7 +59,15 @@ def hsv2rgb3d(vol):
     return np.array([R,G,B])
 
 def convertToFan(vec, halfSphere=False, weights=None, mask=None):
-    """Converts a volume of vectors to a volume of rgba values representing vector directions."""
+    """Converts a volume of vectors to a volume of rgba values representing vector directions using a Fan color scheme.\n
+    Useful for vectors that don't point up or down.\n
+    Params:\n
+    vec - (3,x,y,z) volume of vectors\n
+    halfSphere - if True, the color range is squished to half a sphere\n
+    weights - weights used for the alpha channel\n
+    mask - binary mask of areas of interest\n
+    Returns: (4,x,y,z) rgba volume\n
+    """
 
     if halfSphere:
         # Stretch artificially the x values to use all colors
@@ -78,7 +91,14 @@ def convertToFan(vec, halfSphere=False, weights=None, mask=None):
     return colormap_vol
 
 def convertToIco(vec,  weights=None, mask=None):
-    """Converts a volume of vectors to a volume of rgba values representing vector directions."""
+    """Converts a volume of vectors to a volume of rgba values representing vector directions using an Icosahedron color scheme.\n
+    Useful for vectors that don't have any particular distribution.\n
+    Params:\n
+    vec - (3,x,y,z) volume of vectors\n
+    weights - weights used for the alpha channel\n
+    mask - binary mask of areas of interest\n
+    Returns: (4,x,y,z) rgba volume\n
+    """
 
     coloring = scmap.Ico() 
     vec_flip = np.moveaxis(vec.reshape(3,-1), 0,-1)
@@ -96,34 +116,38 @@ def convertToIco(vec,  weights=None, mask=None):
 
     return colormap_vol
 
-def saveRgbaVolume(rgba,savePath=None):
-    
+def saveRgbaVolume(rgba,savePath):
+    """Prepares and saves RGBA .tiff volume to be viewed in ParaView\n
+    Due to ParaView behaviour that this function corrects for, it may not perfrom as well in other programs\n
+    Params:\n
+    rgba - (4,x,y,z) rgba volume \n
+    savePath - path where the volume should be saved\n
+    """
+
     rgba = np.moveaxis(rgba,0,-1)
     #  ParaView prefers to work with 8 bit uints
     rgba = (rgba*255).astype(np.uint8)
     # # For some reason alpha channel is in the opposite order in ParaView
-    # rgba = 255-rgba
     rgba[:,:,:,3] = 255-rgba[:,:,:,3]
     # ParaView needs a full range of values to work correctly
     rgba[0,0,0,3] = 255
     rgba[1,0,0,3] = 0
     #Save as tiff
-    if savePath is not None:
-        assert os.path.dirname(savePath) == '' or os.path.exists(os.path.dirname(savePath)), f"Given path does not exist {savePath}"
-        _, ext = os.path.splitext(savePath)
-        assert ext == '.tiff', f"File extension ({ext}) is not .tiff"
+    assert os.path.dirname(savePath) == '' or os.path.exists(os.path.dirname(savePath)), f"Given path does not exist {savePath}"
+    _, ext = os.path.splitext(savePath)
+    assert ext == '.tiff', f"File extension ({ext}) is not .tiff"
 
-        #Switched to tiff save from vtk for faster visualization (vtk small for big volumes)
-        # vtk_write_lite.save_rgba2vtk(colormap_vol, savePath)
-        tifffile.imwrite(savePath, rgba)
-
-    # temp = np.moveaxis(colormap_vol.astype('float32'),0,-1)
-    # temp[:,:,:,:-1] = temp[:,:,:,:-1]*255
-    # temp[0,0,0,3] = 255
-    # temp[0,0,0,1] = 0
-    # tifffile.imwrite('temp.tiff', (temp).astype(np.uint8))
+    tifffile.imwrite(savePath, rgba)
 
 def holeFillGauss(vol, thresh=5, minBlobSize=50):
+    """Finds and fills dark holes in the cheese volume using gaussian noise\n
+    Gaussian distribution tries to follow the data distribution\n
+    Params:\n
+    vol - (x,y,z) grayscale volume \n
+    thresh - binarization thershold for finding the holes\n
+    minBlobSize - minimum hole size that will won't be filtered out\n
+    Returns: volume with filled holes, mask with the found holes set to 0
+    """
 
     #Threshold volume and closing
     holeMask = vol < thresh
